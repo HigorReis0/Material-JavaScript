3de8e9878062fda5450ab118959819f05005a51c
Higor <higorreis0@gmail.com>
2025-08-20 21:39:37 -0300

    exercecicios_realizados
    Foram realizados as listas do material enumerado de 1 a 5

--- feito_exercicios_operadores_aritmeticos.js
+++ feito_exercicios_operadores_aritmeticos.js
@@ -5,31 +5,45 @@ Exercícios Práticos - Operadores Aritméticos
 */
 
 // 1. Declare duas variáveis `x = 12` e `y = 4`. Exiba a soma no console.
+const x = 12;
+const y = 4;
 
-
+console.log(x + y); // 16
 // 2. Exiba a subtração de `x - y`.
-
+console.log(x - y); // 8
 
 // 3. Exiba a multiplicação de `x * y`.
-
+console.log(x * y); // 48
 
 // 4. Exiba a divisão de `x / y`.
+console.log(x / y); // 3
 
 
 // 5. Exiba o resto da divisão de `x % y`.
+console.log(x % y); // 0
 
 
 // 6. Exiba o resultado de `x ** y`.
+console.log(x ** y); // 20736
 
 
 // 7. Crie uma variável `n = 7`. Aplique `n++` e mostre antes e depois no console.
-
+var n = 7
+console.log(n); // 7
+n++
+console.log(n); // 8
 
 // 8. Crie uma variável `m = 9`. Aplique `m--` e mostre antes e depois no console.
-
+var m = 7
+console.log(m); // 9
+m--
+console.log(m); // 8
 
 // 9. Resolva a expressão `(5 + 2) * 3 - 4 / 2` e exiba o resultado.
-
+console.log((5 + 2) * 3 - 4 / 2) // 19
 
 // 10. Crie as variáveis `a = 2`, `b = 3`. Calcule `a + b * 2 ** 2 - 5`.
-// Peça para o aluno prever o valor antes de executar.
+const a = 2
+const b = 3
+
+console.log(a + b * 2 ** 2 - 5);
